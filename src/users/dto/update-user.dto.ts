3a8785811a6c809d559acb008351467963994fbc
bishwashgh <104993926+bishwashgh@users.noreply.github.com>
2026-05-24 16:44:28 +0000

    Update update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,21 +1,20 @@
-import { IsOptional, IsEnum } from 'class-validator';
-import { Column,PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
 import { Role } from '../enums/role.enums';
 
 export class UpdateUserDto {
-    @PrimaryGeneratedColumn()
-    id!: number;
-    
-    @Column()
-    name!: string;
-    
-    @Column({ unique: true })
-    email!: string;
-    
-    @Column({ nullable: true})
-    password!: string;
-    
-    @IsOptional()
-    @IsEnum(Role)
-    role?: Role;
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  password?: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
 }
