ca9ee49abc33813bb94781abb52d8883237f6bf1
bishwashgh <bishwashghimire2060@gmail.com>
2026-05-25 21:19:54 +0545

    Fix from path/win32 to path

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
-import { join } from 'path/win32';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
